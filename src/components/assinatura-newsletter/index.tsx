a1f487ebb0541b2223c3877e20d402e58f6eb7ae
Marcus Vinicius Gomes <marcusvinicius3002@gmail.com>
2022-04-02 16:30:22 -0300

    adding type email to email input and adding an alert when sending email

--- src/components/assinatura-newsletter/index.tsx
+++ src/components/assinatura-newsletter/index.tsx
@@ -1,11 +1,20 @@
+import { useRef } from 'react';
 import { StrongElsie } from '../../Global/Styles';
 import { FormSignUpNewsletter } from './styles';
 
 export default function AssinaturaNewsletter() {
+    const emailField = useRef<null | HTMLInputElement>(null);
+
     function handleSignUpNewsletter (e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
 
+        const email = emailField.current!.value;
+        
+        emailField.current!.value = "";
+
         // register e-mail...
+        
+        alert(`Obrigado pela sua assinatura, você receberá nossas novidades no e-mail ${email}.`);
     }
 
     return (
@@ -22,7 +31,13 @@ export default function AssinaturaNewsletter() {
                 `}
             />
             <div>
-                <input type="email" required placeholder="Insira seu e-mail" />
+                <input
+                    ref={emailField}
+                    required
+                    placeholder="Insira seu e-mail"
+                    type="email"
+                />
+
                 <button type="submit" children="Assinar newsletter" />
             </div>
         </FormSignUpNewsletter>
